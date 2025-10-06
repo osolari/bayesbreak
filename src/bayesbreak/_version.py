2db3a097db1226d0fe7dbed17e8f6c1bcad9b551
omid <solari@berkeley.edu>
2025-10-06 12:36:56 -0700

    version

--- src/bayesbreak/_version.py
+++ src/bayesbreak/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.0.dev0.post30+dirty'
-__version_tuple__ = version_tuple = ('0.0.0.dev0.post30+dirty',)
+__version__ = version = '0.1.0.post0+dirty'
+__version_tuple__ = version_tuple = (0, 1, 0, 'post0', 'dirty')
 
-__commit_id__ = commit_id = 'gd440487da'
+__commit_id__ = commit_id = 'g639f5786c'
